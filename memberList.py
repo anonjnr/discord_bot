7a8f490efabd4edb2785275afc5e46a9010b2330
x3l51 <31654136+x3l51@users.noreply.github.com>
2018-08-19 16:20:38 +0200

    Pulls members only from where it's triggered

--- memberList.py
+++ memberList.py
@@ -11,15 +11,13 @@ async def membersDump(ctx):
 
 
 async def membersLog(ctx):
-    for server in ctx.bot.servers:
-        for i, member in enumerate(server.members):
-            list_mem_num = (f'{i}')
-            list_mem_id = (f'{member.id}')
-            list_mem = (f'{member}')
-            list_mem_name = (f'{member.name}')
-            list_all = (f'Number: {list_mem_num} ID: {list_mem_id} Name: {list_mem} ({list_mem_name})\n')
-            print(list_all)
-            with open('./logs/members.log', 'a') as file:
-                file.write(list_all)
+    for i, member in enumerate(ctx.message.server.members):
+        list_mem_num = (f'{i}')
+        list_mem_id = (f'{member.id}')
+        list_mem = (f'{member}')
+        list_mem_name = (f'{member.name}')
+        list_all = (f'Number: {list_mem_num} ID: {list_mem_id} Name: {list_mem} ({list_mem_name})\n')
+        with open('./logs/members.log', 'a') as file:
+            file.write(list_all)
 
     await ctx.bot.say('List saved into logs.')
